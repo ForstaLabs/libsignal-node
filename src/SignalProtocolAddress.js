42e5893539bec3b044b5aee9b6e9776810c8891f
Justin Mayfield <tooker@gmail.com>
2016-12-04 14:13:41 -0700

    Fix export of signalprotocoladdress

--- src/SignalProtocolAddress.js
+++ src/SignalProtocolAddress.js
@@ -19,7 +19,7 @@ SignalProtocolAddress.prototype = {
   }
 };
 
-libsignal.SignalProtocolAddress = function(name, deviceId) {
+const _SignalProtocolAddress = function(name, deviceId) {
   var address = new SignalProtocolAddress(name, deviceId);
 
   ['getName', 'getDeviceId', 'toString', 'equals'].forEach(function(method) {
@@ -27,10 +27,12 @@ libsignal.SignalProtocolAddress = function(name, deviceId) {
   }.bind(this));
 };
 
-libsignal.SignalProtocolAddress.fromString = function(encodedAddress) {
+_SignalProtocolAddress.fromString = function(encodedAddress) {
   if (typeof encodedAddress !== 'string' || !encodedAddress.match(/.*\.\d+/)) {
     throw new Error('Invalid SignalProtocolAddress string');
   }
   var parts = encodedAddress.split('.');
   return new libsignal.SignalProtocolAddress(parts[0], parseInt(parts[1]));
 };
+
+module.exports = _SignalProtocolAddress;
