215e405447c793a5c3ff3c891cbad5a58d85a7a9
Justin Mayfield <tooker@gmail.com>
2016-12-14 17:14:42 -0700

    Converting more subtle api to native node.

--- src/crypto.js
+++ src/crypto.js
@@ -17,75 +17,74 @@ function encrypt(key, data, iv) {
     });
 }
 
-function decrypt(key, data, iv) {
+async function decrypt(key, data, iv) {
     const decipher = node_crypto.createDecipheriv('aes-256-cbc',
                                                   Buffer.from(key),
                                                   Buffer.from(iv));
-    var dec = Buffer.concat([decipher.update(Buffer.from(data)) , decipher.final()]);
-    return dec;
+    return Buffer.concat([
+        decipher.update(Buffer.from(data)),
+        decipher.final()
+    ]);
     //return subtle.importKey('raw', key, {name: 'AES-CBC'}, false,
      //                       ['decrypt']).then(function(key) {
       //  return subtle.decrypt({name: 'AES-CBC', iv: new Uint8Array(iv)}, key, data);
     //});
 }
 
-function sign(key, data) {
-    return subtle.importKey('raw', key, {name: 'HMAC', hash: {name: 'SHA-256'}},
-                            false, ['sign']).then(function(key) {
-        return subtle.sign( {name: 'HMAC', hash: 'SHA-256'}, key, data);
-    });
+async function sign(key, data) {
+    const hmac = node_crypto.createHmac('sha256', Buffer.from(key));
+    hmac.update(Buffer.from(data));
+    return hmac.digest();
 }
 
 function hash(data) {
-    return subtle.digest({name: 'SHA-512'}, data);
+    const h = subtle.digest({name: 'SHA-512'}, data);
+    console.log(h);
+    throw new Error('hash check');
 }
 
 // HKDF for TextSecure has a bit of additional handling.
 // Salts always end up being 32 bytes
-function HKDF(input, salt, plaininfo) {
+async function HKDF(input, salt, plaininfo) {
     // Specific implementation of RFC 5869 that only returns the first 3 32-byte chunks
     // TODO: We dont always need the third chunk, we might skip it
     if (salt.byteLength != 32) {
         throw new Error("Got salt of incorrect length");
     }
     const info = helpers.toArrayBuffer(plaininfo);
-    console.log('INTO THE BREACH!', info, plaininfo);
-    return sign(salt, input).then(function(PRK) {
-        var infoBuffer = new ArrayBuffer(info.byteLength + 1 + 32);
-        var infoArray = new Uint8Array(infoBuffer);
-        infoArray.set(new Uint8Array(info), 32);
-        infoArray[infoArray.length - 1] = 1;
-        return sign(PRK, infoBuffer.slice(32)).then(function(T1) {
-            infoArray.set(new Uint8Array(T1));
-            infoArray[infoArray.length - 1] = 2;
-            return sign(PRK, infoBuffer).then(function(T2) {
-                infoArray.set(new Uint8Array(T2));
-                infoArray[infoArray.length - 1] = 3;
-                return sign(PRK, infoBuffer).then(function(T3) {
-                    return [ T1, T2, T3 ];
-                });
-            });
-        });
-    });
+    const PRK = await sign(salt, input);
+    const infoBuffer = new ArrayBuffer(info.byteLength + 1 + 32);
+    const infoArray = new Uint8Array(infoBuffer);
+    infoArray.set(new Uint8Array(info), 32);
+    infoArray[infoArray.length - 1] = 1;
+    const T1 = await sign(PRK, infoBuffer.slice(32));
+    infoArray.set(new Uint8Array(T1));
+    infoArray[infoArray.length - 1] = 2;
+    const T2 = await sign(PRK, infoBuffer);
+    infoArray.set(new Uint8Array(T2));
+    infoArray[infoArray.length - 1] = 3;
+    const T3 = await sign(PRK, infoBuffer);
+    return [T1, T2, T3];
 }
 
-function verifyMAC(data, key, mac, length) {
-    return sign(key, data).then(function(calculated_mac) {
-        if (mac.byteLength != length  || calculated_mac.byteLength < length) {
-            throw new Error("Bad MAC length");
-        }
-        var a = new Uint8Array(calculated_mac);
-        var b = new Uint8Array(mac);
-        var result = 0;
-        for (var i=0; i < mac.byteLength; ++i) {
-            result = result | (a[i] ^ b[i]);
-        }
-        if (result !== 0) {
-            console.log('Our MAC  ', ByteBuffer.wrap(calculated_mac).toHex());
-            console.log('Their MAC', ByteBuffer.wrap(mac).toHex());
-            throw new Error("Bad MAC");
-        }
-    });
+async function verifyMAC(data, key, mac, length) {
+    const calculated_mac = await sign(key, data);
+    if (mac.byteLength != length  || calculated_mac.byteLength < length) {
+        throw new Error("Bad MAC length");
+    }
+    var a = new Uint8Array(calculated_mac.slice(0, length));
+    var b = new Uint8Array(mac);
+    var result = 0;
+    /* TODO: Optimize this.  This is a pointless exercise in bit manipulation.
+     * Just throw the error if the intersection is not equal. */
+    for (var i=0; i < mac.byteLength; ++i) {
+        result = result | (a[i] ^ b[i]);
+    }
+    if (result !== 0) {
+        console.log('Our MAC  ', a);
+        console.log('Their MAC', b);
+        throw new Error("Bad MAC");
+    }
 }
 
 function getRandomBytes(len) {
