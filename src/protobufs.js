3e18700efd5e780bd0a789f1bef2e2bf36ad87a7
Gary Button <garyhbutton@gmail.com>
2019-01-31 18:06:16 -0800

    Bugfix for dealing with electron

--- src/protobufs.js
+++ src/protobufs.js
@@ -2,7 +2,12 @@
 
 const protobuf = require('protobufjs');
 
-const protodir = __dirname + '/../protos/';
+let dirname = __dirname;
+if (dirname === "/") {
+  dirname = path.dirname(module.i)
+}
+
+const protodir = dirname + '/../protos/';
 const p = protobuf.loadSync(protodir + 'WhisperTextProtocol.proto').lookup('textsecure');
 
 module.exports = {
