e2f1e66dd2113d33a53675ff29d88df691797982
Justin Mayfield <tooker@gmail.com>
2019-02-03 14:39:33 -0700

    Use higher compat path joining for protobufs
    Possibly address issues with electron envs as described in #5

--- src/protobufs.js
+++ src/protobufs.js
@@ -1,9 +1,10 @@
 'use strict';
 
+const path = require('path');
 const protobuf = require('protobufjs');
 
-const protodir = __dirname + '/../protos/';
-const p = protobuf.loadSync(protodir + 'WhisperTextProtocol.proto').lookup('textsecure');
+const protodir = path.resolve(__dirname, '../protos/');
+const p = protobuf.loadSync(path.join(protodir, 'WhisperTextProtocol.proto')).lookup('textsecure');
 
 module.exports = {
     WhisperMessage: p.lookup('WhisperMessage'),
